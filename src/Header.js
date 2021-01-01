515ca6451ce3db4a6e3b422c2a85cfe11941c06d
github kakerutokunaga <icecream.tokeru@gmail.com>
2021-01-01 21:11:06 +0900

    add resetcss

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 
 export default class Header extends Component {
   render() {
-    return <div>this is header</div>;
+    return (
+      <div className="header">
+        <h1 className="title">リマインダー</h1>
+        <p className="todo-counter">TODO: 3</p>
+      </div>
+    );
   }
 }
